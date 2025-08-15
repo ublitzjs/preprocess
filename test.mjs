58517776b5d2975e39ede0d3e41f056d6ca13b7a
Guthib-of-Dan <diril656@gmail.com>
2025-08-15 19:34:59 -0200

    updated npx command

--- test.mjs
+++ test.mjs
@@ -2,14 +2,13 @@
 import {exec} from "node:child_process";
 import { existsSync } from "node:fs";
 import {arch, platform} from "node:os"
-import {argv} from "node:process";
+import {argv, argv0} from "node:process";
 const wantedExe = "./"+platform()+"-"+arch()+".exe";
 if(!existsSync(wantedExe)) {
   console.log("using fallback js implementation, because there is no right binary for " + wantedExe + " yet");
-} else {
-  console.log("Argv: ", argv);
+} else {argv.splice(0,2);
   exec(wantedExe + " " + argv.join(" "), (err,stdout, stderr)=>{
-    if(err) console.error("ERR: ", stderr);
+    if(err) console.error("ERR: ", err, stderr);
     else console.log("successful execution: ", stdout);
   });
 }
