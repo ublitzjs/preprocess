b258cce51b2d695d12bc61a9ae77c6e06e1f087d
Guthib-of-Dan <diril656@gmail.com>
2025-08-15 19:51:31 -0200

    Updated binary

--- test.mjs
+++ test.mjs
@@ -2,13 +2,14 @@
 import {exec} from "node:child_process";
 import { existsSync } from "node:fs";
 import {arch, platform} from "node:os"
-import {argv, argv0} from "node:process";
+import {argv} from "node:process";
 const wantedExe = "./"+platform()+"-"+arch()+".exe";
 if(!existsSync(wantedExe)) {
   console.log("using fallback js implementation, because there is no right binary for " + wantedExe + " yet");
 } else {argv.splice(0,2);
+  console.log("ARGV: ", argv, '\n');
   exec(wantedExe + " " + argv.join(" "), (err,stdout, stderr)=>{
-    if(err) console.error("ERR: ", err, stderr);
-    else console.log("successful execution: ", stdout);
+    if(err) console.error("Exe error: ",stderr);
+    else console.log("Exe success:", stdout);
   });
 }
