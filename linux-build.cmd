53c25b759d97be64484cd9feb9253248aa816d06
Guthib-of-Dan <diril656@gmail.com>
2025-08-15 18:44:16 -0200

    Fixed submodule include for linux build

--- linux-build.cmd
+++ linux-build.cmd
@@ -1,1 +1,1 @@
-g++ -std=c++17 -I./stringzilla/include/ -o preprocess.exe -DPLATFORM_APPROACH=2 src/main.cpp src/worker.cpp
+g++ -std=c++17 -I./Stringzilla/include/ -o preprocess.exe -DPLATFORM_APPROACH=2 src/main.cpp src/worker.cpp
