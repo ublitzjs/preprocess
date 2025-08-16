b46db20c1bc577bcf58d5b3da8a87224d560cea2
Guthib-of-Dan <diril656@gmail.com>
2025-08-16 07:48:38 -0200

    Now binaries work

--- test.mjs
+++ test.mjs
@@ -5,11 +5,11 @@ import {arch, platform} from "node:os"
 import {argv} from "node:process";
 const wantedExe = "./"+platform()+"-"+arch()+".exe";
 if(!existsSync(wantedExe)) {
-  console.log("using fallback js implementation, because there is no right binary for " + wantedExe + " yet");
-} else {argv.splice(0,2);
-  console.log("ARGV: ", argv, '\n');
-  exec(wantedExe + " " + argv.join(" "), (err,stdout, stderr)=>{
-    if(err) console.error("Exe error: ",stderr);
-    else console.log("Exe success:", stdout);
+  console.log("Currently there is no "+wantedExe+" for your platform");
+} else {
+  argv.splice(0,2);
+  exec(wantedExe + " " + argv.map((arg)=>"\""+arg+"\"").join(" "), (err,stdout, stderr)=>{
+    if(err) console.error("Exe error:\n",stderr);
+    else console.log("Exe success:\n", stdout);
   });
 }
