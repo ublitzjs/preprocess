60c01f1541080f8a02d6e1394c51dc00a9e88e8a
Guthib-of-Dan <diril656@gmail.com>
2025-08-28 18:31:35 +0000

    Changed c++ version to be 17 in build.mjs

--- build.mjs
+++ build.mjs
@@ -3,7 +3,7 @@ import {exec} from "node:child_process";
 import {log} from "node:console";
 var compiler = platform() == "darwin" ? "clang++" : "g++";
 var platformApproach = platform() == "win32" ? 0 : 1;
-exec(compiler + " -s -O3 -o " + "./binary.exe -DPLATFORM_APPROACH=" + platformApproach + " -I./StringZilla/include src/main.cpp src/worker.cpp", (err, stdout, stderr)=>{
+exec(compiler + " -std=c++17 -s -O3 -o " + "./binary.exe -DPLATFORM_APPROACH=" + platformApproach + " -I./StringZilla/include src/main.cpp src/worker.cpp", (err, stdout, stderr)=>{
   log("ERR", err);
   log("stdout: ", stdout);
   log("stderr: ", stderr);
