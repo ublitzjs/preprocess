8b1e07a6d5460e330da01955801bde0384bb54dd
Guthib-of-Dan <diril656@gmail.com>
2025-08-28 20:13:58 +0000

    Initialised bundling npx command

--- build.mjs
+++ build.mjs
@@ -3,10 +3,10 @@ import {exec} from "node:child_process";
 import {log} from "node:console";
 var compiler = platform() == "darwin" ? "clang++" : "g++";
 var platformApproach = platform() == "win32" ? 0 : 1;
-exec(compiler + " -std=c++17 -s -O3 -o " + "./binary.exe -DPLATFORM_APPROACH=" + platformApproach + " -I./StringZilla/include src/main.cpp src/worker.cpp", (err, stdout, stderr)=>{
+
+exec(compiler + " -std=c++17 -s -O3 -o " + "./binary.bin -DPLATFORM_APPROACH=" + platformApproach + " -I./StringZilla/include src/main.cpp src/worker.cpp", (err, stdout, stderr)=>{
   log("ERR", err);
   log("stdout: ", stdout);
   log("stderr: ", stderr);
 })
-
 
---
+++ bundle-npx.mjs
@@ -0,0 +1,4 @@
+import {buildSync} from "esbuild";
+buildSync({
+
+})
